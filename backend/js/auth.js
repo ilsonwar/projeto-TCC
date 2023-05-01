0fc21c875d998fcc563e42296964d029bf3a93be
ilsonwar <ilsonwarsc@gmail.com>
2023-05-01 16:23:54 -0300

    feat: Gerenciando estado de autenticação e mostrando loading

--- backend/js/auth.js
+++ backend/js/auth.js
@@ -1,21 +1,26 @@
 authForm.onsubmit = function (event) {
+    showItem(loading)
     event.preventDefault()
     if (authForm.submitAuthForm.innerHTML == 'LOGIN') {
-      firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).then(function (user) {
-        console.log('Acessou com sucesso')
-        console.log(user)
-      }).catch(function (error) {
+      firebase.auth().signInWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
         console.log('Falha no acesso')
         console.log(error)
       })
     } else {
-      firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).then(function (user) {
-        console.log('Cadastrou com sucesso')
-        console.log(user)
-      }).catch(function (error) {
+      firebase.auth().createUserWithEmailAndPassword(authForm.email.value, authForm.password.value).catch(function (error) {
         console.log('Falha no cadastro')
         console.log(error)
       })
-  
     }
   }
+  
+  firebase.auth().onAuthStateChanged(function (user) {
+    hideItem(loading)
+    if (user) {
+      console.log('Usuário autenticado')
+      console.log(user)
+    } else {
+      console.log('Usuário não autenticado')
+    }
+  })
+  
